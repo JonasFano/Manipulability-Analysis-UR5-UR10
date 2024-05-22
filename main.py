7af919db24380aab5411d49349e2d033971a6a6f
Jonas Fano <et20022@lehre.dhbw-stuttgart.de>
2024-05-22 17:06:33 +0200

    Update and record data with correct tcp position

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from spatialmath import SE3, SO3
 from point_cloud_manipulability import Point_cloud_Manipulability
 from swift import Swift
 from tqdm import tqdm
+import time
 
 def home_pos(robot):
     # Move to some initial joint position
@@ -13,7 +14,7 @@ def home_pos(robot):
     home_q = np.deg2rad([-60, -120, -110, -40, 90, 0])
     robot.q = home_q
 
-    # env.step()
+    env.step()
 
     T_base_tcp = robot.fkine(robot.q, end="tool0")  # forward kinematics
     # print(T_base_tcp)
@@ -21,8 +22,8 @@ def home_pos(robot):
 
 
 # Instantiate the graphical environment (Swift) and launch
-# env = Swift()
-# env.launch()
+env = Swift()
+env.launch()
 
 # # Instantiate a UR5 robot
 # robot = rtb.models.URDF.UR5()
@@ -33,13 +34,33 @@ def home_pos(robot):
 # input("Press enter to start robot manipulability check")
 
 
-for z in tqdm(np.arange(0.15, 0.4, 0.05), desc="Z Loop", leave = False):
-    robot = rtb.models.URDF.UR5()
+
+# for z in tqdm(np.arange(0.1, 0.5, 0.05), desc="Z Loop", leave = False):
+#     robot = rtb.models.URDF.UR5()
+#     T_robot = SE3.Tz(z)
+#     robot.base = T_robot * robot.base # Transform robot base position
+
+#     for y in tqdm(np.arange(0.3, 0.65, 0.05), desc="Y Loop", leave = False):
+#         for x in tqdm(np.arange(-0.1, 0.2, 0.05), desc="X Loop", leave = False):
+
+
+
+# for z in tqdm(np.arange(0.3, 0.6, 0.05), desc="Z Loop", leave = False):
+#     robot = rtb.models.URDF.UR10()
+#     T_robot = SE3.Tz(z)
+#     robot.base = T_robot * robot.base # Transform robot base position
+
+#     for y in tqdm(np.arange(0.5, 1.0, 0.05), desc="Y Loop", leave = False):
+#         for x in tqdm(np.arange(-0.3, 0.4, 0.05), desc="X Loop", leave = False):
+
+
+for z in tqdm(np.arange(0.5, 0.52, 0.05), desc="Z Loop", leave = False):
+    robot = rtb.models.URDF.UR10()
     T_robot = SE3.Tz(z)
     robot.base = T_robot * robot.base # Transform robot base position
 
-    for y in tqdm(np.arange(0.4, 0.55, 0.05), desc="Y Loop", leave = False):
-        for x in tqdm(np.arange(-0.1, 0.2, 0.05), desc="X Loop", leave = False):
+    for y in tqdm(np.arange(0.7, 0.92, 0.05), desc="Y Loop", leave = False):
+        for x in tqdm(np.arange(-0.3, -0.28, 0.05), desc="X Loop", leave = False):
             # Create an empty list to store manipulability values
             manipulability_values = []
             success_list = []
@@ -47,16 +68,17 @@ for z in tqdm(np.arange(0.15, 0.4, 0.05), desc="Z Loop", leave = False):
 
             # Set the new position of the body
             T_base_belly = SE3(x, y, 0)
-            # print(T_base_belly)
 
-            belly = sg.Mesh(filename="Belly_new.stl", scale=[1, 1.0, 1.0], base=T_base_belly, collision=True)
+            belly = sg.Mesh(filename="belly.stl", scale=[1.0, 1.0, 1.0], base=T_base_belly, collision=True)
 
-            # env.add(robot)
+            env.add(robot)
             T_base_tcp = home_pos(robot)
-            # env.add(belly)
+            env.add(belly)
+
+            time.sleep(20.0)
 
             point_cloud = Point_cloud_Manipulability()
-            point_cloud.load_from_object_file(stl_file_name="Belly_new.stl", obj_translate=[x, y, 0.0], scale_factor=1, num_points=3500)
+            point_cloud.load_from_object_file(stl_file_name="belly.stl", obj_translate=[x, y, 0.0], scale_factor=1, num_points=3500)
             point_cloud.sample_points_above_z(z_threshold=0.15)
 
             points = np.asarray(point_cloud.filtered_point_cloud.points)
@@ -91,20 +113,16 @@ for z in tqdm(np.arange(0.15, 0.4, 0.05), desc="Z Loop", leave = False):
                 # T_point_tcp = SE3.Rt(T_base_tcp.R, [x, y, 0.31])# + 0.15175])
                 T_point_tcp = SE3.Rt(rotation_matrix, np.array(points[i]))
 
-                # print(T_point_tcp)
                 translation = SE3.Tz(-0.15) 
                 T_point_tcp = T_point_tcp * translation
-                # print(T_point_tcp)
 
-                T_point_tcp = SE3.Tz(z) * T_point_tcp # To account for a different robot base position
-                # print(T_point_tcp)
+                T_point_tcp = SE3.Tz(-z) * T_point_tcp # To account for a different robot base position
 
                 # Inverse Kinematics on robot
                 q, success, iter, searches, residual = robot.ik_LM(T_point_tcp, q0=np.deg2rad([-60, -120, -110, -40, 90, 0]), end='tool0')
                 robot.q = q
-                # print(robot.fkine(q))
 
-                # env.step()
+                env.step()
 
                 # Check for collisions
                 iscollision = robot.iscollided(q=robot.q, shape=belly) # boolean
@@ -121,10 +139,13 @@ for z in tqdm(np.arange(0.15, 0.4, 0.05), desc="Z Loop", leave = False):
                 
                 #input("Press enter to move to next point")
 
+                # Wait for half a second after each step
+                time.sleep(0.5)
+
             
             #input("Press enter to move to change belly position")
             # Save the NumPy array to a file
-            # np.savetxt("data_UR5/manipulability_values_" + str(x) + "_" + str(y) + "_" + str(z) + ".txt", np.array(manipulability_values))
-            # np.savetxt("data_UR5/success_list_" + str(x) + "_" + str(y) + "_" + str(z) + ".txt", np.array(success_list))
-            # np.savetxt("data_UR5/collision_list_" + str(x) + "_" + str(y) + "_" + str(z) + ".txt", np.array(collision_list))
+            # np.savetxt("data_UR10_v5/manipulability_values_" + str(x) + "_" + str(y) + "_" + str(z) + ".txt", np.array(manipulability_values))
+            # np.savetxt("data_UR10_v5/success_list_" + str(x) + "_" + str(y) + "_" + str(z) + ".txt", np.array(success_list))
+            # np.savetxt("data_UR10_v5/collision_list_" + str(x) + "_" + str(y) + "_" + str(z) + ".txt", np.array(collision_list))
             # env.reset()
--- plot_results.py
+++ plot_results.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def plot_all_data_UR5():
-    for z in np.arange(0.15, 0.4, 0.05):
-        for y in np.arange(0.4, 0.55, 0.05):
+    for z in np.arange(0.1, 0.5, 0.05):
+        for y in np.arange(0.3, 0.65, 0.05):
             for x in np.arange(-0.1, 0.2, 0.05):
                 # Load the data from the text files
                 manipulability_values = np.loadtxt("data_UR5/manipulability_values_" + str(x) + "_" + str(y) + "_" + str(z) + ".txt")
@@ -32,9 +32,9 @@ def plot_all_data_UR5():
 
 
 def plot_all_data_UR10():
-    for z in np.arange(0.0, 0.5, 0.1):
-        for y in np.arange(0.5, 0.8, 0.1):
-            for x in np.arange(-0.2, 0.3, 0.1):
+    for z in np.arange(-0.1, 0.5, 0.05):
+        for y in np.arange(0.5, 0.9, 0.05):
+            for x in np.arange(-0.2, 0.3, 0.05):
                 # Load the data from the text files
                 manipulability_values = np.loadtxt("data_UR10/manipulability_values_" + str(x) + "_" + str(y) + "_" + str(z) + ".txt")
                 success_list = np.loadtxt("data_UR10/success_list_" + str(x) + "_" + str(y) + "_" + str(z) + ".txt")
@@ -44,7 +44,7 @@ def plot_all_data_UR10():
                 plt.figure(figsize=(10, 5))
                 plt.subplot(1, 2, 1)
                 plt.plot(manipulability_values)
-                plt.title("Manipulability Values (x={:.1f}, y={:.1f}, z={:.1f})".format(x, y, z))
+                plt.title("Manipulability Values (x={:.2f}, y={:.2f}, z={:.2f})".format(x, y, z))
                 plt.xlabel("Index")
                 plt.ylabel("Manipulability")
 
@@ -52,7 +52,7 @@ def plot_all_data_UR10():
                 plt.subplot(1, 2, 2)
                 plt.plot(success_list, label="Success")
                 plt.plot(collision_list, label="Collision")
-                plt.title("Success and Collision (x={:.1f}, y={:.1f}, z={:.1f})".format(x, y, z))
+                plt.title("Success and Collision (x={:.2f}, y={:.2f}, z={:.2f})".format(x, y, z))
                 plt.xlabel("Index")
                 plt.ylabel("Boolean")
                 plt.legend()
@@ -61,8 +61,8 @@ def plot_all_data_UR10():
                 plt.show()
 
 def plot_filtered_data_UR5():
-    for z in np.arange(0.15, 0.4, 0.05):
-        for y in np.arange(0.4, 0.55, 0.05):
+    for z in np.arange(0.1, 0.5, 0.05):
+        for y in np.arange(0.3, 0.65, 0.05):
             for x in np.arange(-0.1, 0.2, 0.05):
                 # Load the data from the text files
                 manipulability_values = np.loadtxt("data_UR5/manipulability_values_" + str(x) + "_" + str(y) + "_" + str(z) + ".txt")
@@ -93,9 +93,9 @@ def plot_filtered_data_UR5():
 
 
 def plot_filtered_data_UR10():
-    for z in np.arange(0.0, 0.5, 0.1):
-        for y in np.arange(0.5, 0.8, 0.1):
-            for x in np.arange(-0.2, 0.3, 0.1):
+    for z in np.arange(0.3, 0.6, 0.05):
+        for y in np.arange(0.5, 1.0, 0.05):
+            for x in np.arange(-0.3, 0.4, 0.05):
                 # Load the data from the text files
                 manipulability_values = np.loadtxt("data_UR10/manipulability_values_" + str(x) + "_" + str(y) + "_" + str(z) + ".txt")
                 success_list = np.loadtxt("data_UR10/success_list_" + str(x) + "_" + str(y) + "_" + str(z) + ".txt")
@@ -107,7 +107,7 @@ def plot_filtered_data_UR10():
                     plt.figure(figsize=(10, 5))
                     plt.subplot(1, 2, 1)
                     plt.plot(manipulability_values)
-                    plt.title("Manipulability Values (x={:.1f}, y={:.1f}, z={:.1f})".format(x, y, z))
+                    plt.title("Manipulability Values (x={:.2f}, y={:.2f}, z={:.2f})".format(x, y, z))
                     plt.xlabel("Index")
                     plt.ylabel("Manipulability")
 
@@ -115,7 +115,7 @@ def plot_filtered_data_UR10():
                     plt.subplot(1, 2, 2)
                     plt.plot(success_list, label="Success")
                     plt.plot(collision_list, label="Collision")
-                    plt.title("Success and Collision (x={:.1f}, y={:.1f}, z={:.1f})".format(x, y, z))
+                    plt.title("Success and Collision (x={:.2f}, y={:.2f}, z={:.2f})".format(x, y, z))
                     plt.xlabel("Index")
                     plt.ylabel("Boolean")
                     plt.legend()
@@ -123,6 +123,46 @@ def plot_filtered_data_UR10():
                     plt.tight_layout()
                     plt.show()
 
+
+def plot_filtered_data_UR10_few():
+    c = 0
+    for z in np.arange(0.3, 0.6, 0.05):
+        for y in np.arange(0.5, 1.0, 0.05):
+            for x in np.arange(-0.3, 0.4, 0.05):
+                # Load the data from the text files
+                manipulability_values = np.loadtxt("data_UR10/manipulability_values_" + str(x) + "_" + str(y) + "_" + str(z) + ".txt")
+                success_list = np.loadtxt("data_UR10/success_list_" + str(x) + "_" + str(y) + "_" + str(z) + ".txt")
+                collision_list = np.loadtxt("data_UR10/collision_list_" + str(x) + "_" + str(y) + "_" + str(z) + ".txt")
+
+                # Check if there are no 0 values in success_list or no 1 values in collision_list
+                # and there are 5 or fewer collisions
+                if np.sum(collision_list) <= 5 and np.sum(success_list == 0) <= 5:
+                    c += 1
+                    # # Plot manipulability values
+                    # plt.figure(figsize=(10, 5))
+                    # plt.subplot(1, 2, 1)
+                    # plt.plot(manipulability_values)
+                    # plt.title("Manipulability Values (x={:.2f}, y={:.2f}, z={:.2f})".format(x, y, z))
+                    # plt.xlabel("Index")
+                    # plt.ylabel("Manipulability")
+
+                    # # Plot success list
+                    # plt.subplot(1, 2, 2)
+                    # plt.plot(success_list, label="Success")
+                    # plt.plot(collision_list, label="Collision")
+                    # plt.title("Success and Collision (x={:.2f}, y={:.2f}, z={:.2f})".format(x, y, z))
+                    # plt.xlabel("Index")
+                    # plt.ylabel("Boolean")
+                    # plt.legend()
+
+                    # plt.tight_layout()
+                    # plt.show()
+    print(c)
+
+
 if __name__ == "__main__":
-    plot_all_data_UR5()
+    # plot_all_data_UR5()
+    # plot_all_data_UR10()
+    # plot_filtered_data_UR10()
     # plot_filtered_data_UR5()
+    plot_filtered_data_UR10_few()
